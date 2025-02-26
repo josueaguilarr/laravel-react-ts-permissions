dfc7928ca7b0e092eed7ee2552ace4ee71f66249
josueaguilarr <josue.aguilar225@gmail.com>
2025-02-25 22:32:58 -0500

    Initial commit 🧡

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,49 @@
+import { type SharedData } from '@/types';
+import { Head, Link, usePage } from '@inertiajs/react';
+
+export default function Welcome() {
+    const { auth } = usePage<SharedData>().props;
+
+    return (
+        <>
+            <Head title="Welcome">
+                <link rel="preconnect" href="https://fonts.bunny.net" />
+                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+            </Head>
+            <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
+                <div className="min-h-[400px] flex flex-col w-full items-center justify-center gap-y-10">
+                    <p
+                        className="text-transparent bg-clip-text font-bold text-7xl bg-gradient-to-r from-neutral-800 via-neutral-200 to-neutral-800 dark:from-neutral-200 dark:via-neutral-800 dark:to-neutral-200 animate-shine"
+                    >
+                        Start easily with security
+                    </p>
+                    <nav className="flex items-center justify-end gap-4">
+                        {auth.user ? (
+                            <Link
+                                href={route('dashboard')}
+                                className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                            >
+                                Dashboard
+                            </Link>
+                        ) : (
+                            <>
+                                <Link
+                                    href={route('login')}
+                                    className="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                                >
+                                    Log in
+                                </Link>
+                                <Link
+                                    href={route('register')}
+                                    className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                                >
+                                    Register
+                                </Link>
+                            </>
+                        )}
+                    </nav>
+                </div>
+            </div>
+        </>
+    );
+}
